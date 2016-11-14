98c837ffcb61fe76083fb2afc611a54277bb1208
Omkar Hatalkar <omkarh@gmail.com>
2016-11-14 15:00:24 +0530

    Added ng-route and ng-provider

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,24 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.controller('appController', ['$scope', function ($scope) {
-	$scope.message = "welcome to my app";
+myApp.config(['$routeProvider', function ($routeProvider) {
+	
+	$routeProvider.
+		when('/login', {
+			templateUrl: 'views/login.html',
+			controller: 'RegistrationController'
+		}).
+		when('/register', {
+			templateUrl: 'views/register.html',
+			controller: 'RegistrationController'
+		}).
+		when('/success', {
+			templateUrl: 'views/success.html',
+			controller: 'SuccessController'
+		}).
+		otherwise({
+			redirectTo: '/login'
+		});
+
 }]);
 
 
